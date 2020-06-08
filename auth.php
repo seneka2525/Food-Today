1728a52fa6f125f7475a15c5691341a88e5a5a15
seneka0124 <stillaki2@gmail.com>
2020-06-08 10:20:41 +0900

    ログアウトページ完成

--- auth.php
+++ auth.php
@@ -7,7 +7,7 @@ if(!empty($_SESSION['login_date'])){
   debug('ログイン済みユーザーです。');
 
   // 現在日時が最終ログイン日時＋有効期限を超えていた場合
-  if( ($_SESSION['login_date'] + $_SESSION['login_limit']) > time()){
+  if( ($_SESSION['login_date'] + $_SESSION['login_limit']) < time()){
     debug('ログイン有効期限オーバーです。');
 
     // セッションを削除（ログアウトする）
@@ -18,11 +18,22 @@ if(!empty($_SESSION['login_date'])){
     debug('ログイン有効期限以内です');
     // 最終ログイン日時を現在日時に更新
     $_SESSION['login_date'] = time();
+
+    // ================== 無限ループ対策 ==================
+    // 偏在実行中のスクリプトファイル名がlogin.phpの場合
+    // $_SERVER['PHP_SELF']はドメインからのパスを返すため、今回だと「/eattoday/login.php」が返ってくるので、
+    // さらにbasename関数を使うことでファイル名だけを取り出せる
+    if(basename($_SERVER['PHP_SELF']) === 'login.php'){
     debug('マイページへ遷移します。');
     header("Location:mypage.php"); //マイページへ
+    }
+
   }
 
 }else{
   debug('未ログインユーザーです。');
+  if(basename($_SERVER['PHP_SELF']) !== 'login.php'){
+    header("Location:login.php"); //ログインページへ
+  }
 }
 ?>
