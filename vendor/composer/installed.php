33f0452e6e7d560410ec971df3435e9887ebb788
seneka2525 <stillaki2@gmail.com>
2022-05-07 11:23:56 +0900

    composer.jsonファイル変更

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'faa7cd09d73ee97d868fd77bde37dcf3f69587ca',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'faa7cd09d73ee97d868fd77bde37dcf3f69587ca',
+            'dev_requirement' => false,
+        ),
+    ),
+);
